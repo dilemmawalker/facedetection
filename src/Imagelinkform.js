3f2d8621aec9a3694c647f5d88931fce3980f184
yash handa <yashhanda500@gmail.com>
2020-12-11 17:11:39 +0530

    update

--- src/Imagelinkform.js
+++ src/Imagelinkform.js
@@ -1,9 +1,17 @@
 
+function move(props,prop2){
+    return(
+console.log(props,prop2)
+    );
+}
+
 function Imagelinkform(){
     return(
         <div>
                 <input type="text" style={{width:"400px"}} class={{}}  /><br></br><br></br>
-                <button>detect</button>
+                <button onClick={
+                 ()=>  move('const','fuck')
+                }>detect</button>
         </div>
     );
 }
