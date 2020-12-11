c6de302b485c08332e82880f6ae3eeeb0e6cf333
yash handa <yashhanda500@gmail.com>
2020-12-11 20:30:48 +0530

    update

--- src/Imagelinkform.js
+++ src/Imagelinkform.js
@@ -5,10 +5,36 @@ console.log(props,prop2)
     );
 }
 
+function move2(prop){
+    // return(
+    //     console.log(prop.target.value)
+    // );
+    return(
+        stub.PostModelOutputs(
+            {
+                model_id: "{THE_MODEL_ID}",
+                inputs: [
+                    {data: {image: {url: "https://samples.clarifai.com/metro-north.jpg"}}}
+                ]
+            },
+            metadata,
+            (err, response) => {
+                console.log("Predicted concepts:");
+                for (const concept of response.outputs[0].data.concepts) {
+                    console.log(concept.name + " " + concept.value);
+                }
+            }
+        )
+    );
+}
+
 function Imagelinkform(){
     return(
         <div>
-                <input type="text" style={{width:"400px"}} class={{}}  /><br></br><br></br>
+                <input id='aa' type="text" style={{width:"400px"}} class={{}} 
+                onChange={
+                   (i)=> move2(i)
+                } /><br></br><br></br>
                 <button onClick={
                  ()=>  move('const','fuck')
                 }>detect</button>
